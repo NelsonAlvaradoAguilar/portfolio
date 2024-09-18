781ac7763cff17b6e0c36a53242f5a7762792676
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-09-17 22:15:33 -0400

    adding header mobile for education and toggle buttons

--- src/componentes/AboutMe/AboutMe.js
+++ src/componentes/AboutMe/AboutMe.js
@@ -9,14 +9,16 @@ function AboutMe(params) {
     <section className="bio">
       {aboutMeDada?.map((data) => {
         return (
-          <div className="bio" key={data.id}>
+          <div className="bio__container" key={data.id}>
             <div className="bio__subcontainer">
               <img className="bio__image" src={data.image}></img>
               <h1>{data.name}</h1>
             </div>
             <article className="bio__article">
-              <p>{data.paragraph1}</p>
-              <p>{data.paragraph2}</p>
+              <p className="bio__paragraph">{data.paragraph1}</p>
+              <p className="bio__paragraph bio__paragraph--botton">
+                {data.paragraph2}
+              </p>
             </article>
           </div>
         );
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,13 @@
+import AboutMe from "../../componentes/AboutMe/AboutMe";
+import Education from "../../componentes/Education/Education";
+
+function Main(params) {
+  return (
+    <>
+      <AboutMe />
+      <Education />
+    </>
+  );
+}
+
+export default Main;
