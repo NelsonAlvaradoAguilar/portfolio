6be8aa095ed697560274edc76be9ce7c07682631
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-09-21 17:20:33 -0400

    imgc  again

--- src/componentes/Institutions/Institutions.js
+++ src/componentes/Institutions/Institutions.js
@@ -1,44 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Institutions.scss";
 import diploma from "../../assets/ imageDiploma.png";
-import certification from "../../assets/certification.png";
-import colombianCertificate from "../../assets/colombian certificate.png";
 import { useEffect, useState } from "react";
-import Image from "../Image/Image";
-function Institution({ institution, Open, id }) {
-  console.log(id);
-
+function Institution({ institution, Open, index }) {
   return (
     <section className={`institutions ${!Open ? "institutions__hide" : ""}`}>
-      {id === "0" ? (
-        <img
-          className="institutions__image"
-          alt={institution?.school}
-          src={diploma}
-        />
-      ) : (
-        ""
-      )}
-      {id === "1" ? (
-        <img
-          className="institutions__image"
-          alt={institution?.school}
-          src={certification}
-        />
-      ) : (
-        ""
-      )}
-      {id === "2" ? (
-        <img
-          className="institutions__image"
-          alt={institution?.school}
-          src={colombianCertificate}
-        ></img>
-      ) : (
-        ""
-      )}
       <ul className="institutions__container">
         <li className="institutions__list ">
+          {institution?.image ? (
+            <img
+              className="institutions__image"
+              alt={institution?.school}
+              src={institution?.image}
+            />
+          ) : (
+            <p>No image available</p>
+          )}
           <div>
             <h3>
               {institution?.school}
@@ -51,9 +28,7 @@ function Institution({ institution, Open, id }) {
           </div>
         </li>
       </ul>
-      {id === "1" ? <img width={200} src={diploma}></img> : ""}
     </section>
   );
 }
 export default Institution;
-//  {id === "1" ? <img width={200} src={diploma}></img> : ""}
