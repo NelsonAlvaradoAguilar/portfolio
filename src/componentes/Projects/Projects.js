36925369d58b00f7582b99ddd76c45c71f8727a5
NelsonAlvaradoAguilar <nelsonalvaradoaguilar@gmail.com>
2024-10-03 00:06:09 -0400

    still workining on projects section

--- src/componentes/Projects/Projects.js
+++ src/componentes/Projects/Projects.js
@@ -1,123 +1,66 @@
 import React, { useEffect, useState } from "react";
 import "./Projects.scss"; // Create a separate SCSS file for Project styling
-
+import ProjectsUl from "../ProjectsUl/ProjectsUl";
+import ProjectsDetails from "../ProjectsDetails/ProjectsDetails";
 import { projects, dropDown } from "../../apiData/data";
 const Project = () => {
-  const [project, setProject] = useState([]);
-  const [googleFit, setGoogleFit] = useState([]);
+  const [project, setProject] = useState([] || null);
   const [open, setOpen] = useState(false);
+
   useEffect(() => {
-    setProject(projects[0].capstone);
-    setGoogleFit(projects[1].googleFit);
+    setProject(projects);
     console.log(project);
   }, []);
 
-  function openProject(params) {
-    setOpen(true);
-    if (open) {
-      setOpen(false);
-    }
+  function openProject(selectedId) {
+    const selectedProject = projects.find((proj) => proj.id === selectedId);
+    console.log(selectedProject);
+    setProject(selectedProject);
+    console.log(project);
+
     console.log(open);
+    setOpen(true);
+
+    return project;
   }
+  const close = () => {
+    console.log("Closing project details"); // Add this log
+    setOpen(false);
+  };
   return (
     <section className="project">
       <h1
         className="project__title"
-        onClick={() => {
-          openProject();
+        onClick={(id) => {
+          //  openProject(id);
         }}
       >
         Projects{" "}
       </h1>
 
-      {!open
-        ? project?.map((capstone, index) => (
-            <ul key={index} className="project__ul">
-              <li className="project__ul--li">
-                <h2 className="project__title">
-                  {capstone.projectTitle}{" "}
-                  {!open ? (
-                    <img
-                      onClick={() => {
-                        openProject();
-                      }}
-                      className={`project__dropdown ${
-                        open ? "project__dropdown--down" : ""
-                      }`}
-                      src={dropDown}
-                    ></img>
-                  ) : (
-                    ""
-                  )}
-                </h2>
-              </li>
-              <li>
-                <div className="project__images">
-                  {capstone?.images[1] && (
-                    <img
-                      className="project__image"
-                      src={capstone.images[4]}
-                      alt={`Project ${capstone.projectTitle} Image 2`}
-                    />
-                  )}
-                </div>
-              </li>
-            </ul>
-          ))
-        : null}
-      {!open
-        ? googleFit?.map((googleFit, index) => (
-            <ul key={index} className="project__ul">
-              <li className="project__ul--li">
-                <h2 className="project__title">{googleFit.projectTitle}</h2>
-              </li>
-              <li>
-                <div className="project__images">
-                  {googleFit?.images[0] && (
-                    <img
-                      className="project__image"
-                      src={googleFit.images[0]}
-                      alt={`Project ${googleFit.projectTitle} Image 2`}
-                    />
-                  )}
-                </div>
-              </li>
-            </ul>
-          ))
-        : null}
+      {project?.map((proj) => (
+        <ProjectsUl
+          key={proj.id}
+          data={proj}
+          open={open}
+          openProject={openProject}
+          dropDown={dropDown}
+        />
+      ))}
 
-      {open
-        ? project?.map((capstone, index) => (
-            <ul key={index} className="project__ul">
-              <li className="project__ul--li">
-                <h2 className="project__title">{capstone.projectTitle}</h2>
-                <p className="project__description">{capstone.description}</p>
-              </li>
-              <li>
-                <div className="project__images">
-                  {capstone?.images?.map((image, imgIndex) => (
-                    <img
-                      key={imgIndex}
-                      className="project__image"
-                      src={image}
-                      alt={`Project ${capstone.projectTitle} Image ${imgIndex}`}
-                    />
-                  ))}
-                </div>
-              </li>
-            </ul>
-          ))
-        : null}
       {open ? (
-        <img
-          onClick={() => {
-            openProject();
-          }}
-          className={`project__dropdown ${
-            open ? "project__dropdown--down" : ""
-          }`}
-          src={dropDown}
-        ></img>
+        <>
+          <ProjectsDetails details={project} setOpen={setOpen} />
+
+          <img
+            onClick={close}
+            className={`project__dropdown ${
+              open ? "project__dropdown--down" : ""
+            }`}
+            src={dropDown}
+            alt="Close projects dropdown"
+          />
+        </>
       ) : (
         ""
       )}
@@ -133,4 +76,7 @@ export default Project;
               src={image}
               alt={`Project ${project.projectTitle} Image ${index + 1}`}
             />
-          ))} */
+          ))}  setOpen(true);
+    if (open) {
+      setOpen(false);
+    }*/
